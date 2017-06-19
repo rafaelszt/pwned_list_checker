1bb87d2dc36c5cd7c51b0210647069df3236fc86
Rafael Teixeira <rafael.szt@gmail.com>
2017-06-19 14:39:42 -0300

    Added progress bar (kinda broken), fixed some stuff, move stuff around, changed some code

--- pypwned.py
+++ pypwned.py
@@ -2,12 +2,33 @@ import requests
 import sys
 import time
 import getopt
-import json
 
 
 PWNED_BREACH_API_URL = "https://haveibeenpwned.com/api/v2/breachedaccount/%s"
 PWNED_BREACH_SHORT_API_URL = "https://haveibeenpwned.com/api/v2/breachedaccount/%s?truncateResponse=true"
 PWNED_PASTE_API_URL = "https://haveibeenpwned.com/api/v2/pasteaccount/%s"
+
+
+class ProgressBar():
+    previous_value = -1
+
+    def __init__(self):
+        self.toolbar_width = 100
+
+        sys.stdout.write("[%s]" % (" " * self.toolbar_width))
+        sys.stdout.flush()
+        sys.stdout.write("\b" * (self.toolbar_width+1)) # return to start of line, after '['
+
+
+    def update(self, value):
+        if (value != self.previous_value):
+            self.previous_value = value
+            sys.stdout.write("-")
+            sys.stdout.flush()
+
+
+    def finish():
+        sys.stdout.write("\n")
 
 
 def load_file(dir):
@@ -24,6 +45,9 @@ def load_file(dir):
 
 
 class InvalidEmail(Exception):
+    pass
+
+class ToManyRequests(Exception):
     pass
 
 
@@ -43,23 +67,33 @@ def response(req):
 
     # To many requests, slowing down
     elif req.status_code == 429:
-        raise Exception('ToManyRequests')
+        raise ToManyRequests("To many requests")
+
+    else:
+        print ("\
+               Something went wrong.\n\
+               Try again later. \n\
+               Or right now, it's up to you.")
+        sys.exit(2)
 
 
 def check_breach(email, long_version=False):
     req = ''
+
     if long_version:
         req = requests.get(PWNED_BREACH_API_URL % email)
+
     else:
         req = requests.get(PWNED_BREACH_SHORT_API_URL % email)
 
     try:
         r = response(req)
-
-    except:
-        retry_after = int(req.headers['Retry-After'])
-        time.sleep (retry_after + 0.1)
+    except ToManyRequests:
+        print ("Too many requests, waiting a few seconds before trying again.")
+        time.sleep (int(req.headers['Retry-After']) + 0.1)
         r = check_breach(email, long_version)
+    except:
+        sys.exit(2)
 
     return r
 
@@ -70,54 +104,55 @@ def check_paste(email):
     try:
         r = response(req)
 
-    except:
-        retry_after = int(req.headers['Retry-After']) / 1000
-        time.sleep (retry_after + 0.1)
+    except ToManyRequests:
+        time.sleep (int(req.headers['Retry-After']) + 0.1)
         r = check_paste(email)
+    except:
+        sys.exit(2)
 
     return r
 
+
+def single_mail(mail, long_version):
+    breach = False
+    paste = False
+
+    pwned = check_breach(mail, long_version)
+    if len(pwned) > 0:
+        breach = True
+
+    pwned = check_paste(mail)
+    if len(pwned) > 0:
+        paste = True
+
+    return breach, paste
 
 def mail_list(list, long_version):
     breach_list = []
     paste_list = []
 
+    total_processed = 0
+    progress_bar = ProgressBar()
+
     for mail in list:
+        breach, paste = single_mail(mail, long_version)
+
         # Did the email have any leaks?
-        pwned = check_breach(mail, long_version)
-        if (len(pwned) > 0):
-            breach_list.append(pwned)
+        if (breach):
+            breach_list.append(mail)
 
         # What about in the pastes?
-        pwned = check_paste(mail)
-        if (len(pwned) > 0):
-            paste_list.append(pwned)
+        if (paste):
+            paste_list.append(mail)
+
+        # We can't forget about our progress bar
+        total_processed += 1
+        progress_bar.update(int(total_processed / len(list)*100))
 
         # Let's not abuse the API
         time.sleep(1.5)
 
     return breach_list, paste_list
-
-
-def single_mail(mail):
-    breach = []
-    paste = []
-
-    pwned = check_breach(mail, False)
-    if len(pwned) > 0:
-        breach = pwned
-
-    pwned = check_paste(mail)
-    if len(pwned) > 0:
-        paste = pwned
-
-    return breach, paste
-
-
-def print_json(list):
-    for l in list:
-        print (json.dumps(list, sort_keys=False,
-                          indent=4, separators=None))
 
 
 def show_options():
@@ -133,7 +168,7 @@ def main(argv):
     paste_list = []
 
     try:
-        opts, args = getopt.getopt(argv,"hl:s:c",["list=","single="])
+        opts, args = getopt.getopt(argv,"hl:s:",["list=","single="])
 
     except getopt.GetoptError:
         show_options()
@@ -151,22 +186,24 @@ def main(argv):
         elif opt in ('-l', '--list'):
             mails = load_file(arg)
             breach_list, paste_list = mail_list(mails, False)
-            if len(breach_list) > 0 or len(paste_list) > 0:
-                print(arg)
-                pwned_mails.append(arg)
+
+            break
 
         elif opt in ('-s', '--single'):
-            breach_list, paste_list = single_mail(arg)
-            if len(breach_list) > 0 or len(paste_list) > 0:
-                pwned_mails.append(arg)
+            breach_list, paste_list = single_mail(arg, False)
+            if breach_list:
+                breach_list = arg
+            if paste_list:
+                paste_list = arg
 
-        if opt in ('-c'):
-            # if we got some pwned users, get more info on then
-            if pwned_mails is not []:
-                pwned_mails = mail_list(pwned_mails, True)
-                print_json(pwned_mails)
-        else:
-            print (pwned_mails)
+            break
+
+    if len(breach_list) > 0:
+        print ("Emails with a breach:")
+        print (breach_list)
+    if len(paste_list) > 0:
+        print ("Emails in a paste:")
+        print (paste_list)
 
 
 if __name__ == "__main__":
